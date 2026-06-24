57000b428ebceef32e650ea3bc9899b2039ad9e1
akeredolu <akeredolu.waheed@gmail.com>
2026-06-24 13:17:40 +0100

    Prioritize staticfiles application order to fix admin styling collection

--- core/settings.py
+++ core/settings.py
@@ -35,13 +35,13 @@ ALLOWED_HOSTS = [
 # APPLICATIONS
 # ==========================================================
 INSTALLED_APPS = [
-    "cloudinary_storage",
+    "django.contrib.staticfiles",  
+    "cloudinary_storage",          
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     # Third-party
     "rest_framework",
     "corsheaders",
